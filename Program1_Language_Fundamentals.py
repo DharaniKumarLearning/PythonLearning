324b1e56f1b13bef07033aa14a3cd6805bf472ef
Dharani <dharanik22@apple.com>
2024-02-28 21:32:05 +0530

    Language Fundamentals Start

--- Program1_Language_Fundamentals.py
+++ Program1_Language_Fundamentals.py
@@ -1,2 +1,41 @@
-print("Hello World")
-print()
+"""
+
+Some theory about Python Programming Language :-
+------------------------------------------------
+
+When compared with other programming languages Python is very easy to learn
+Python is a general purpose high level programming language
+General purpose :- can be used to develop data analysis applications, web-application, desktop applications etc.,
+Human understandable languages are high level programming languages
+
+Python was developed by Guido Van Rossam in 1989 while working at National Research Institute in netherlands
+Officially Python was made available to public in Feb 20th, 1991
+
+C and Java are statically typed programming language (the variable type should be declared before using it)
+Python is dynamically typed programming language (no need to declare type of variable before using it)
+    Based on the value we assign to the variable the type of the variable will be determined
+
+Features of Python :-
+---------------------
+
+1. Simple and easy to learn
+2. Freeware(no need to pay anything to use the software) and open source(we can see the
+source code of the functions and customise it if we want to)
+3. Python is both procedure oriented and object-oriented programming language
+4. Python is interpreted programming language
+5. Python has rich library support which makes it easy to code complex logics
+
+Python Versions :-
+-------------------
+
+Python 1.0 introduced in Jan 1994
+Python 2.0 introduced in October 2000
+Python 3.x introduced in December 2008 -- highly recommended to use this version
+
+In general any new s/w version should provide backward compatibility for older version programs.
+Python 3.x doesn't provide backward compatibility for older version programs.
+
+print "Hello" -- valid in Python 2.0 but invalid in Python 3.x
+long data type is present in Python 2 but not available in Python 3.x
+
+"""
