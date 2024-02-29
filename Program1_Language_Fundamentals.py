e58672b81d1858809170276ad3e6bf4776bf68a4
Dharani <dharanik22@apple.com>
2024-02-29 07:58:53 +0530

    Language Fundamentals Start

--- Program1_Language_Fundamentals.py
+++ Program1_Language_Fundamentals.py
@@ -39,3 +39,58 @@ print "Hello" -- valid in Python 2.0 but invalid in Python 3.x
 long data type is present in Python 2 but not available in Python 3.x
 
 """
+
+import math  # math is called a module which is basically a group of functions, variables and classes
+from random import *
+import keyword
+
+print('Hello World in Python')
+a, b = 10, 20  # we can declare multiple variables on the same line
+print(f'Sum of {a} and {b} is :- {a + b}')
+a, b, c, d, e = 100, 200, 300, 400, 500
+print(f'Sum of the variables declared in the same line is : {a+ b + c + d + e}')
+
+print(f'The type of the variable a is : {type(a)}')
+a = True  # we can make a variable point to a different point of object which is perfectly fine
+print(f'The type of the variable after pointing to a different type of object is : {type(a)}')
+
+print(f'Square root of the number 90 is :- {math.sqrt(90)}')
+print(f'Using randint function to generate six digit random number')
+
+for i in range(1,10):
+    print(randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9)
+          , randint(0, 9), randint(0, 9), sep='')
+
+"""
+
+Identifiers in Python :-
+------------------------
+
+Any name in Python program is called identifier.
+It can be a variable name, function name, class name etc.,
+
+Rules to define an identifier :-
+--------------------------------
+
+1. The only allowed symbols are alphabets(both uppercase and lowercase), digits(0-9) and underscore(_).
+2. Identifiers should not start with a digit.
+3. Python identifiers are case sensitive. total and TOTAL are two different variables.
+4. We can not use keywords(reserved words) like if, else ,def, class etc., as identifiers.
+5. There is no length limit for Python identifier.
+
+"""
+
+total, TOTAL = 100, 200
+print(f'The value of the total is {total}')
+print(f'The value of the TOTAL is {TOTAL}')
+
+# If the identifier starts with underscore symbol it indicates that it is private
+# If the identifier starts with two underscore symbols then it is strongly private
+# If the identifier starts and ends with two underscore symbols then it is language specific identifie
+
+print(f'All the keywords present in the current version of Python are :- {keyword.kwlist}')
+
+x = 10
+print(f'The value of x is {x}')
+del x  # deleting the variable so that it is eligible for garbage collection
+# print(f'The value of x after deletion is {x}')  # NameError: name 'x' is not defined
